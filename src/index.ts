2ee2b60226b2ff24a4d9287e689c4e49997a3cbc
Diego Morales <diego.mm09@hotmail.com>
2022-05-05 18:34:56 -0500

    Primer Commit

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+//Primero
+/*
+const myPromise = Promise.resolve(1)
+myPromise.then((value) => {
+  console.log(value) // log: 1
+})
+*/
+//Segundo
+/*
+const myPromise = new Promise((resolve) => {
+  resolve(myPromise) ;
+})
+
+myPromise.catch((value) => {
+ console.log(value + 1)  // log: 2
+})
+*/
+
+//Tercero
+/*
+const myPromise = new Promise((resolve) => {
+  resolve(1)
+})
+
+myPromise
+  .then((value) => {
+    return value + 2
+  })
+  .then((value) => {
+   console.log(value + ???) // log: 4
+  })
+*/
+
+//Cuarto
+/*
+const myPromise = new Promise((resolve, reject) => {
+  console.log('Error: Operation Failed')
+})
+
+myPromise
+  .catch((err) => {
+   console.log(err) // log: 'Error: Operation Failed'
+  })
+*/
+
+//Quinto
+const callback1 = (value: any) => console.log('Esto es un texto:', value)
+const callback2 = (value: any) => console.log('Esto es un numero:', value)
+
+const myPromise = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(2);
+}, 1000)
+});
+
+myPromise.then(callback2); // log: Esto es un numero: 2
